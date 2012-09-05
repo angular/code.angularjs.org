c52b0470e7816a1c2e3933b322530918b8d0d477
Miško Hevery <misko@hevery.com>
2012-09-05 13:25:41 -0700

    Update fetchLatestSnapshot.php
    Corrected a concatenation typo 

--- fetchLatestSnapshot.php
+++ fetchLatestSnapshot.php
@@ -11,22 +11,24 @@
     if ( !$ver or !preg_match("/^[\.\-\d\w]+$/", $ver) ) {
       echo 'ERROR: No version specified';
     } else {
+      echo 'Fetching: <a href="'.$url.'">'.$url.'</a><br>';
       shell_exec('wget '.$url.' -O angular-snapshot.zip');
       if ( !file_exists('angular-snapshot.zip') ) {
         echo 'Error: Invalid URL: <a href="'.$url.'">'.$url.'</a>';
       } else {
-        if( !($out = shell_exec('unzip angular-snapshot.zip')) ) {
+        $out = shell_exec('unzip angular-snapshot.zip');
+        if( !$out ) {
           echo 'Error: Not a valid ZIP file';
         } else {
           echo $out;
           if ( !file_exists('angular-'.$ver) ) {
             echo 'Error: Zip file does not contain angular-'.$ver.' directory.';
           } else {
-            `mv snapshot snapshot.old`;
-            `mv angular-' . $ver . ' snapshot`;
-            `rm -rf angular-snapshot.zip`;
-            `rm -rf snapshot.old`;
-            
+            shell_exec('mv snapshot snapshot.old');
+            shell_exec('mv angular-' . $ver . ' snapshot');
+            shell_exec('rm -rf angular-snapshot.zip');
+            shell_exec('rm -rf snapshot.old');
+
             echo 'docs.angularjs.org now serving: '.$ver;
           }
         }
