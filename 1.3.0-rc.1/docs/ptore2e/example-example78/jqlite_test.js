8c356db9b8d85f9b59efc198057ba295242bb1b5
jenkins <ci-bot@angularjs.org>
2014-09-09 16:01:56 -0700

    v1.3.0-rc.1

---
+++ 1.3.0-rc.1/docs/ptore2e/example-example78/jqlite_test.js
@@ -0,0 +1,12 @@
+describe("directive", function() {
+  var rootEl;
+  beforeEach(function() {
+    rootEl = browser.rootEl;
+    browser.get("./examples/example-example78/index.html");
+  });
+  
+  it('should show off bindings', function() {
+    expect(element(by.css('div[ng-controller="Controller"] span[ng-bind]')).getText())
+        .toBe('Max Karl Ernst Ludwig Planck (April 23, 1858 – October 4, 1947)');
+  });
+});
