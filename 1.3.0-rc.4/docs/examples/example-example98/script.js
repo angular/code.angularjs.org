882696591036c99b6afd2a3123f41c045c228801
jenkins <ci-bot@angularjs.org>
2014-10-01 21:34:25 -0700

    v1.3.0-rc.4

---
+++ 1.3.0-rc.4/docs/examples/example-example98/script.js
@@ -0,0 +1,15 @@
+  angular.module('myStatefulFilterApp', [])
+    .filter('decorate', ['decoration', function(decoration) {
+
+      function decorateFilter(input) {
+        return decoration.symbol + input + decoration.symbol;
+      }
+      decorateFilter.$stateful = true;
+
+      return decorateFilter;
+    }])
+    .controller('MyController', ['$scope', 'decoration', function($scope, decoration) {
+      $scope.greeting = 'hello';
+      $scope.decoration = decoration;
+    }])
+    .value('decoration', {symbol: '*'});
