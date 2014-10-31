a5fe8c422eb9b5bf9635882c1c8f3105e328c8ab
jenkins <ci-bot@angularjs.org>
2014-10-31 10:46:16 -0700

    v1.3.1

---
+++ 1.3.1/docs/examples/example-example10/script.js
@@ -0,0 +1,14 @@
+  angular.module('docsTemplateUrlDirective', [])
+    .controller('Controller', ['$scope', function($scope) {
+      $scope.customer = {
+        name: 'Naomi',
+        address: '1600 Amphitheatre'
+      };
+    }])
+    .directive('myCustomer', function() {
+      return {
+        templateUrl: function(elem, attr){
+          return 'customer-'+attr.type+'.html';
+        }
+      };
+    });
