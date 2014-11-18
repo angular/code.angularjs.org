5603f42453b8739b6a39bb4c079c25a588a0b9ff
jenkins <ci-bot@angularjs.org>
2014-11-18 00:07:00 -0800

    v1.3.3

---
+++ 1.3.3/docs/ptore2e/example-example101/default_test.js
@@ -0,0 +1,11 @@
+describe("", function() {
+  var rootEl;
+  beforeEach(function() {
+    rootEl = browser.rootEl;
+    browser.get("build/docs/examples/example-example101/index.html");
+  });
+  
+  it('should jsonify filtered objects', function() {
+    expect(element(by.binding("{'name':'value'}")).getText()).toMatch(/\{\n  "name": ?"value"\n}/);
+  });
+});
