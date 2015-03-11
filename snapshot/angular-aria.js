309a5d73e9aa419a67946def5a9ae3b3c2f7bfac
jenkins <ci-bot@angularjs.org>
2015-03-11 06:08:50 -0700

    v1.4.0-build.3895+sha.2b4dfa9

--- snapshot/angular-aria.js
+++ snapshot/angular-aria.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3894+sha.2b4dfa9
+ * @license AngularJS v1.4.0-build.3895+sha.2b4dfa9
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
--- snapshot/docs/js/versions-data.js
+++ snapshot/docs/js/versions-data.js
@@ -7,13 +7,13 @@ angular.module('versionsData', [])
   "patch": 0,
   "prerelease": [
     "build",
-    "3894"
+    "3895"
   ],
   "build": "sha.2b4dfa9",
-  "version": "1.4.0-build.3894",
+  "version": "1.4.0-build.3895",
   "codeName": "snapshot",
   "isSnapshot": true,
-  "full": "1.4.0-build.3894+sha.2b4dfa9",
+  "full": "1.4.0-build.3895+sha.2b4dfa9",
   "branch": "master"
 })
   .value('NG_VERSIONS', [
@@ -24,13 +24,13 @@ angular.module('versionsData', [])
     "patch": 0,
     "prerelease": [
       "build",
-      "3894"
+      "3895"
     ],
     "build": "sha.2b4dfa9",
-    "version": "1.4.0-build.3894",
+    "version": "1.4.0-build.3895",
     "codeName": "snapshot",
     "isSnapshot": true,
-    "full": "1.4.0-build.3894+sha.2b4dfa9",
+    "full": "1.4.0-build.3895+sha.2b4dfa9",
     "branch": "master"
   },
   {
