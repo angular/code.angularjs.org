0a8a4b87ea4b8655a47055749b6c771a69b8d4d1
jenkins <ci-bot@angularjs.org>
2015-04-15 13:50:28 -0700

    v1.4.0-build.3969+sha.1a0bcb1

--- snapshot/angular-loader.js
+++ snapshot/angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.0-build.3968+sha.0d64f08
+ * @license AngularJS v1.4.0-build.3969+sha.1a0bcb1
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -58,7 +58,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.4.0-build.3968+sha.0d64f08/' +
+    message += '\nhttp://errors.angularjs.org/1.4.0-build.3969+sha.1a0bcb1/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
--- snapshot/docs/js/versions-data.js
+++ snapshot/docs/js/versions-data.js
@@ -7,13 +7,13 @@ angular.module('versionsData', [])
   "patch": 0,
   "prerelease": [
     "build",
-    "3968"
+    "3969"
   ],
-  "build": "sha.0d64f08",
-  "version": "1.4.0-build.3968",
+  "build": "sha.1a0bcb1",
+  "version": "1.4.0-build.3969",
   "codeName": "snapshot",
   "isSnapshot": true,
-  "full": "1.4.0-build.3968+sha.0d64f08",
+  "full": "1.4.0-build.3969+sha.1a0bcb1",
   "branch": "master"
 })
   .value('NG_VERSIONS', [
@@ -24,13 +24,13 @@ angular.module('versionsData', [])
     "patch": 0,
     "prerelease": [
       "build",
-      "3968"
+      "3969"
     ],
-    "build": "sha.0d64f08",
-    "version": "1.4.0-build.3968",
+    "build": "sha.1a0bcb1",
+    "version": "1.4.0-build.3969",
     "codeName": "snapshot",
     "isSnapshot": true,
-    "full": "1.4.0-build.3968+sha.0d64f08",
+    "full": "1.4.0-build.3969+sha.1a0bcb1",
     "branch": "master"
   },
   {
