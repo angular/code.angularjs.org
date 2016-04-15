e301e6d51797e8642c6ab73f13a99799e8c42930
jenkins <ci-bot@angularjs.org>
2016-04-15 06:44:40 -0700

    v1.5.5-build.4752+sha.ddac3b3

--- snapshot/angular-loader.js
+++ snapshot/angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.5-build.4751+sha.56861c0
+ * @license AngularJS v1.5.5-build.4752+sha.ddac3b3
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -87,7 +87,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.5-build.4751+sha.56861c0/' +
+    message += '\nhttp://errors.angularjs.org/1.5.5-build.4752+sha.ddac3b3/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
--- snapshot/docs/js/versions-data.js
+++ snapshot/docs/js/versions-data.js
@@ -7,15 +7,15 @@ angular.module('versionsData', [])
   "patch": 5,
   "prerelease": [
     "build",
-    "4751"
+    "4752"
   ],
-  "build": "sha.56861c0",
-  "version": "1.5.5-build.4751",
+  "build": "sha.ddac3b3",
+  "version": "1.5.5-build.4752",
   "codeName": "snapshot",
   "isSnapshot": true,
-  "full": "1.5.5-build.4751+sha.56861c0",
+  "full": "1.5.5-build.4752+sha.ddac3b3",
   "branch": "master",
-  "commitSHA": "56861c0ae9fd52a8d47d4cc5c1db7f4c9b60526b"
+  "commitSHA": "ddac3b3348e0d5cddba81cf1ad6848a23685d8a6"
 })
   .value('NG_VERSIONS', [
   {
@@ -25,15 +25,15 @@ angular.module('versionsData', [])
     "patch": 5,
     "prerelease": [
       "build",
-      "4751"
+      "4752"
     ],
-    "build": "sha.56861c0",
-    "version": "1.5.5-build.4751",
+    "build": "sha.ddac3b3",
+    "version": "1.5.5-build.4752",
     "codeName": "snapshot",
     "isSnapshot": true,
-    "full": "1.5.5-build.4751+sha.56861c0",
+    "full": "1.5.5-build.4752+sha.ddac3b3",
     "branch": "master",
-    "commitSHA": "56861c0ae9fd52a8d47d4cc5c1db7f4c9b60526b"
+    "commitSHA": "ddac3b3348e0d5cddba81cf1ad6848a23685d8a6"
   },
   {
     "raw": "v1.5.4",
