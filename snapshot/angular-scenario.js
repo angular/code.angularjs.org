203a857f8c2285165fdce543e4b00c32e5f9059e
jenkins <ci-bot@angularjs.org>
2016-07-28 02:58:17 -0700

    v1.5.9-build.4962+sha.9ac9fb1

--- snapshot/angular-scenario.js
+++ snapshot/angular-scenario.js
@@ -10071,7 +10071,7 @@ return jQuery;
 } );
 
 /**
- * @license AngularJS v1.5.9-build.4961+sha.c0795c9
+ * @license AngularJS v1.5.9-build.4962+sha.9ac9fb1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -10130,7 +10130,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.5.9-build.4961+sha.c0795c9/' +
+    message += '\nhttp://errors.angularjs.org/1.5.9-build.4962+sha.9ac9fb1/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
@@ -12614,7 +12614,7 @@ function toDebugString(obj) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.5.9-build.4961+sha.c0795c9',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.5.9-build.4962+sha.9ac9fb1',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 5,
   dot: 9,
--- snapshot/docs/js/versions-data.js
+++ snapshot/docs/js/versions-data.js
@@ -7,15 +7,15 @@ angular.module('versionsData', [])
   "patch": 9,
   "prerelease": [
     "build",
-    "4961"
+    "4962"
   ],
-  "build": "sha.c0795c9",
-  "version": "1.5.9-build.4961",
+  "build": "sha.9ac9fb1",
+  "version": "1.5.9-build.4962",
   "codeName": "snapshot",
   "isSnapshot": true,
-  "full": "1.5.9-build.4961+sha.c0795c9",
+  "full": "1.5.9-build.4962+sha.9ac9fb1",
   "branch": "master",
-  "commitSHA": "c0795c97a5cd922722c813a5177c172b6820ed3b"
+  "commitSHA": "9ac9fb1565ea601a16033061d5a460a41bd89f93"
 })
   .value('NG_VERSIONS', [
   {
@@ -25,15 +25,15 @@ angular.module('versionsData', [])
     "patch": 9,
     "prerelease": [
       "build",
-      "4961"
+      "4962"
     ],
-    "build": "sha.c0795c9",
-    "version": "1.5.9-build.4961",
+    "build": "sha.9ac9fb1",
+    "version": "1.5.9-build.4962",
     "codeName": "snapshot",
     "isSnapshot": true,
-    "full": "1.5.9-build.4961+sha.c0795c9",
+    "full": "1.5.9-build.4962+sha.9ac9fb1",
     "branch": "master",
-    "commitSHA": "c0795c97a5cd922722c813a5177c172b6820ed3b"
+    "commitSHA": "9ac9fb1565ea601a16033061d5a460a41bd89f93"
   },
   {
     "raw": "v1.5.8",
