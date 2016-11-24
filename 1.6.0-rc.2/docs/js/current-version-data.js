00e6fcc4b4cee96f7a323802164339d910409111
jenkins <ci-bot@angularjs.org>
2016-11-24 14:04:47 -0800

    v1.6.0-rc.2

---
+++ 1.6.0-rc.2/docs/js/current-version-data.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.6.0-rc.2",
+  "major": 1,
+  "minor": 6,
+  "patch": 0,
+  "prerelease": [
+    "rc",
+    2
+  ],
+  "build": [],
+  "version": "1.6.0-rc.2",
+  "codeName": "safety-insurance",
+  "full": "1.6.0-rc.2",
+  "branch": "v1.6.x",
+  "cdn": {
+    "raw": "v1.5.8",
+    "major": 1,
+    "minor": 5,
+    "patch": 8,
+    "prerelease": [],
+    "build": [],
+    "version": "1.5.8",
+    "docsUrl": "http://code.angularjs.org/1.5.8/docs"
+  }
+});
