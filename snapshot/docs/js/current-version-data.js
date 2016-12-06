d4d1c36682f2322e642da28f799bece80996cb00
jenkins <ci-bot@angularjs.org>
2016-12-06 03:04:07 -0800

    v1.5.10-build.5180+sha.7a667c7

--- snapshot/docs/js/current-version-data.js
+++ snapshot/docs/js/current-version-data.js
@@ -8,13 +8,13 @@ angular.module('currentVersionData', [])
   "patch": 10,
   "prerelease": [
     "build",
-    "5179"
+    "5180"
   ],
-  "build": "sha.752b1e6",
-  "version": "1.5.10-build.5179",
+  "build": "sha.7a667c7",
+  "version": "1.5.10-build.5180",
   "codeName": "snapshot",
   "isSnapshot": true,
-  "full": "1.5.10-build.5179+sha.752b1e6",
+  "full": "1.5.10-build.5180+sha.7a667c7",
   "branch": "master",
   "cdn": {
     "raw": "v1.5.9",
