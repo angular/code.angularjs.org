669f92552e0a9b1220e0e8d52c09e0fd4bf7c483
jenkins <ci-bot@angularjs.org>
2017-01-30 09:41:18 -0800

    v1.6.2-build.5257+sha.03dbd94

--- snapshot/angular-loader.js
+++ snapshot/angular-loader.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.2-build.5256+sha.0ef193f
+ * @license AngularJS v1.6.2-build.5257+sha.03dbd94
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -87,7 +87,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message += '\nhttp://errors.angularjs.org/1.6.2-build.5256+sha.0ef193f/' +
+    message += '\nhttp://errors.angularjs.org/1.6.2-build.5257+sha.03dbd94/' +
       (module ? module + '/' : '') + code;
 
     for (i = SKIP_INDEXES, paramPrefix = '?'; i < templateArgs.length; i++, paramPrefix = '&') {
--- snapshot/docs/js/current-version-data.js
+++ snapshot/docs/js/current-version-data.js
@@ -8,13 +8,13 @@ angular.module('currentVersionData', [])
   "patch": 2,
   "prerelease": [
     "build",
-    "5256"
+    "5257"
   ],
-  "build": "sha.0ef193f",
-  "version": "1.6.2-build.5256",
+  "build": "sha.03dbd94",
+  "version": "1.6.2-build.5257",
   "codeName": "snapshot",
   "isSnapshot": true,
-  "full": "1.6.2-build.5256+sha.0ef193f",
+  "full": "1.6.2-build.5257+sha.03dbd94",
   "branch": "master",
   "cdn": {
     "raw": "v1.6.1",
