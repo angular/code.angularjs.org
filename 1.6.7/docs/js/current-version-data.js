079fd24795dbdb9376b33b6bbbfc7d449d0b0f40
jenkins <ci-bot@angularjs.org>
2017-11-24 10:19:47 -0800

    v1.6.7

---
+++ 1.6.7/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.6.7",
+  "major": 1,
+  "minor": 6,
+  "patch": 7,
+  "prerelease": [],
+  "build": [],
+  "version": "1.6.7",
+  "codeName": "imperial-backstroke",
+  "full": "1.6.7",
+  "branch": "v1.6.x",
+  "cdn": {
+    "raw": "v1.6.6",
+    "major": 1,
+    "minor": 6,
+    "patch": 6,
+    "prerelease": [],
+    "build": [],
+    "version": "1.6.6",
+    "docsUrl": "http://code.angularjs.org/1.6.6/docs"
+  }
+});
