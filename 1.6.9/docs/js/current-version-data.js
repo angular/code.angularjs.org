da86adec1976177ffa6bd6d2e387c40ce294fae8
jenkins <ci-bot@angularjs.org>
2018-02-02 05:13:05 -0800

    v1.6.9

---
+++ 1.6.9/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.6.9",
+  "major": 1,
+  "minor": 6,
+  "patch": 9,
+  "prerelease": [],
+  "build": [],
+  "version": "1.6.9",
+  "codeName": "fiery-basilisk",
+  "full": "1.6.9",
+  "branch": "v1.6.x",
+  "cdn": {
+    "raw": "v1.6.7",
+    "major": 1,
+    "minor": 6,
+    "patch": 7,
+    "prerelease": [],
+    "build": [],
+    "version": "1.6.7",
+    "docsUrl": "http://code.angularjs.org/1.6.7/docs"
+  }
+});
