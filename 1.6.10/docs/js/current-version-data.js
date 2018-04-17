8f6968b12003f3b36c371a63963f9e9130418540
jenkins <ci-bot@angularjs.org>
2018-04-17 10:47:23 -0700

    v1.6.10

---
+++ 1.6.10/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.6.10",
+  "major": 1,
+  "minor": 6,
+  "patch": 10,
+  "prerelease": [],
+  "build": [],
+  "version": "1.6.10",
+  "codeName": "crystalline-persuasion",
+  "full": "1.6.10",
+  "branch": "v1.6.x",
+  "cdn": {
+    "raw": "v1.6.9",
+    "major": 1,
+    "minor": 6,
+    "patch": 9,
+    "prerelease": [],
+    "build": [],
+    "version": "1.6.9",
+    "docsUrl": "http://code.angularjs.org/1.6.9/docs"
+  }
+});
