a9aaaccade0cf218f9af15f755f3e44af9cdae90
jenkins <ci-bot@angularjs.org>
2018-05-11 08:00:41 -0700

    v1.7.0

---
+++ 1.7.0/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.7.0",
+  "major": 1,
+  "minor": 7,
+  "patch": 0,
+  "prerelease": [],
+  "build": [],
+  "version": "1.7.0",
+  "codeName": "nonexistent-physiology",
+  "full": "1.7.0",
+  "branch": "v1.7.x",
+  "cdn": {
+    "raw": "v1.6.9",
+    "major": 1,
+    "minor": 6,
+    "patch": 9,
+    "prerelease": [],
+    "build": [],
+    "version": "1.6.9",
+    "docsUrl": "http://code.angularjs.org/1.6.9/docs"
+  }
+});
