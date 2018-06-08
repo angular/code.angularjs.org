110ac2f12fa79f1e4ea5e548b0c87c68c0624f4e
jenkins <ci-bot@angularjs.org>
2018-06-08 07:31:16 -0700

    v1.7.1

---
+++ 1.7.1/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.7.1",
+  "major": 1,
+  "minor": 7,
+  "patch": 1,
+  "prerelease": [],
+  "build": [],
+  "version": "1.7.1",
+  "codeName": "momentum-defiance",
+  "full": "1.7.1",
+  "branch": "v1.7.x",
+  "cdn": {
+    "raw": "v1.7.0",
+    "major": 1,
+    "minor": 7,
+    "patch": 0,
+    "prerelease": [],
+    "build": [],
+    "version": "1.7.0",
+    "docsUrl": "http://code.angularjs.org/1.7.0/docs"
+  }
+});
