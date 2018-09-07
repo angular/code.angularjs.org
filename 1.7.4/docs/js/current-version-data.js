e750620def21ef03c37f3cb7102dbf49acad0a41
Pete Bacon Darwin <pete@bacondarwin.com>
2018-09-07 10:27:46 +0100

    v1.7.4

---
+++ 1.7.4/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.7.4",
+  "major": 1,
+  "minor": 7,
+  "patch": 4,
+  "prerelease": [],
+  "build": [],
+  "version": "1.7.4",
+  "codeName": "interstellar-exploration",
+  "full": "1.7.4",
+  "branch": "v1.7.x",
+  "cdn": {
+    "raw": "v1.7.2",
+    "major": 1,
+    "minor": 7,
+    "patch": 2,
+    "prerelease": [],
+    "build": [],
+    "version": "1.7.2",
+    "docsUrl": "http://code.angularjs.org/1.7.2/docs"
+  }
+});
