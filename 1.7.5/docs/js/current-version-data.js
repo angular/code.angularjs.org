59861d224332deaa3ead6c8e4cb947053fdf069a
Pete Bacon Darwin <pete@bacondarwin.com>
2018-10-04 15:09:59 +0100

    v1.7.5

---
+++ 1.7.5/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.7.5",
+  "major": 1,
+  "minor": 7,
+  "patch": 5,
+  "prerelease": [],
+  "build": [],
+  "version": "1.7.5",
+  "codeName": "anti-prettification",
+  "full": "1.7.5",
+  "branch": "v1.7.x",
+  "cdn": {
+    "raw": "v1.7.4",
+    "major": 1,
+    "minor": 7,
+    "patch": 4,
+    "prerelease": [],
+    "build": [],
+    "version": "1.7.4",
+    "docsUrl": "http://code.angularjs.org/1.7.4/docs"
+  }
+});
