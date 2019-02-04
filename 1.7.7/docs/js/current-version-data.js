3614a70a9959b847e8975d52e94ad7c954c3ca8d
Pete Bacon Darwin <pete@bacondarwin.com>
2019-02-04 13:17:19 +0000

    v1.7.7

---
+++ 1.7.7/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.7.7",
+  "major": 1,
+  "minor": 7,
+  "patch": 7,
+  "prerelease": [],
+  "build": [],
+  "version": "1.7.7",
+  "codeName": "kingly-exiting",
+  "full": "1.7.7",
+  "branch": "v1.7.x",
+  "cdn": {
+    "raw": "v1.7.6",
+    "major": 1,
+    "minor": 7,
+    "patch": 6,
+    "prerelease": [],
+    "build": [],
+    "version": "1.7.6",
+    "docsUrl": "http://code.angularjs.org/1.7.6/docs"
+  }
+});
