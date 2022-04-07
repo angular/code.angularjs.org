6701d2e24cb891253f9084121661a699e1367707
Jeremy Elbourn <jelbourn@google.com>
2022-04-07 15:10:25 -0700

    v1.8.3

---
+++ 1.8.3/docs/js/current-version-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.8.3",
+  "major": 1,
+  "minor": 8,
+  "patch": 3,
+  "prerelease": [],
+  "build": [],
+  "version": "1.8.3",
+  "codeName": "ultimate-farewell",
+  "full": "1.8.3",
+  "branch": "v1.8.x",
+  "cdn": {
+    "raw": "v1.8.2",
+    "major": 1,
+    "minor": 8,
+    "patch": 2,
+    "prerelease": [],
+    "build": [],
+    "version": "1.8.2",
+    "docsUrl": "http://code.angularjs.org/1.8.2/docs"
+  }
+});
